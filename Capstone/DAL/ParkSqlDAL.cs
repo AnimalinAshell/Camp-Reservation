83080c3d6dc818841ca05145a477325963beed5d
Craig Lawrie <craig.i.lawrie@gmail.com>
2018-02-22 09:38:16 -0500

    park model and base dal

---
+++ Capstone/DAL/ParkSqlDAL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Models;
+using System.Data.SqlClient;
+
+namespace Capstone.DAL
+{
+    public class ParkSqlDAL
+    {
+        private string ConnectionString { get; set; }
+
+        public ParkSqlDAL (string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public List<Park> GetAllParks()
+        {
+            List<Park> parkList = new List<Park>();
+
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FORM park", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Park p = new Park();
+
+                        p.Area = Convert.ToInt32(reader["area"]);
+                        p.Description = Convert.ToString(reader["description"]);
+                        p.Establish_Date = Convert.ToDateTime(reader["establish_date"]);
+                        p.Location = Convert.ToString(reader["location"]);
+                        p.Name = Convert.ToString(reader["name"]);
+                        p.Park_id = Convert.ToInt32(reader["park_id"]);
+                        p.Visitors = Convert.ToInt32(reader["visitors"]);
+
+                        parkList.Add(p);
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Something went wrong reading the database: \n" + e.Message);
+                throw;
+            }
+
+            return parkList;
+        }
+
+    }
+}
---
+++ Capstone/Models/Park.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Park
+    {
+        public int Park_id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime Establish_Date { get; set; }
+        public int Area { get; set; }
+        public int Visitors { get; set; }
+        public string Description { get; set; }
+
+        public string ToString()
+        {
+            return Name + "\n" +
+                   "Locations".PadRight(17) + Location + "\n" +
+                   "Established".PadRight(17) + Establish_Date.ToShortDateString() + "\n" +
+                   "Area".PadRight(17) + Area.ToString() + " sq km" + "\n" +
+                   "Annual Visitors".PadRight(17) + $"{Visitors:#,###,##0}" + "\n\n" +
+                   Description;
+        }
+    }
+}
